30082cde71b7c38ae1c36cbadbb1e2c4ff045069
simoncerman <cerman.s@seznam.cz>
2021-01-11 13:33:15 +0100

    edit to main

--- gamei2a/The game/MainWindow.xaml.cs
+++ gamei2a/The game/MainWindow.xaml.cs
@@ -24,70 +24,127 @@ namespace The_game
     public partial class MainWindow : Window
     {
 
-        //workplace
-        public class walliind
+        public class walliind //vlastní class na zaznamenávání pozice wall 
         {
-            public int wallleft { get; set; }
-            public int wallright { get; set; }
-            public int wallup { get; set; }
-            public int walldown { get; set; }
+            public int wallleft { get; set; } //levá strana stěny 
+            public int wallright { get; set; } //pravá strana stěny 
+            public int wallup { get; set; } //horní strana stěny
+            public int walldown { get; set; } //dolní strana stěny
+            public int eontop { get; set; } //nepřítel typu 1 nad stěnou
         }
-        public static List<walliind> wali = new List<walliind>();
+        public static List<walliind> wali = new List<walliind>(); // vytvoření listu stěn
         //
-        public static int testonly = 0;
-        public static int workinggrid_width = 900;
-        public static int workinggrid_height = 750;
+        public class enemlog //vlastní class na zaznamenání pozic enemie
+        {
+            public int left { get; set; }
+            public int right { get; set; }
+            public int top { get; set; }
+            public int bottom { get; set; }
+
+        }
+        public static List<enemlog> enemiesave = new List<enemlog>();
+        //
+        public static int workinggrid_width = 900; //šířka celého plátna 
+        public static int workinggrid_height = 750; //výška celého plátna
         //character 
-        public static int char_width = 40;
-        public static int char_height = 90;
-        public static int sup = 5;
-        //wall arrays
-        //
+        public static int char_width = 40; //šířka hráče 
+        public static int char_height = 90; //výška hráče 
         character ivan;
         wall wall1;
         wall wall2;
         wall wall3;
         DispatcherTimer time = new DispatcherTimer(DispatcherPriority.Render);
-
+        scoreboard score;
         public MainWindow()
         {
             InitializeComponent();
             time.Interval = new TimeSpan(0, 0, 0, 0, 1);
             time.Tick += Clock;
+
             time.Start();
+            //score 
+            score = new scoreboard(mriz, 800, 40);
+
+
             //object creating
             ivan = new character(mriz);
             // stěny ///
             wall1 = new wall(100, 300, 150, 50, mriz);
             wall2 = new wall(600, 300, 150, 50, mriz);
             wall3 = new wall(350, 500, 150, 50, mriz);
+
             //
 
         }
+        public class scoreboard
+        {
+            public static Label scor;
+            public scoreboard(Grid mriz, int sleft, int stop)
+            {
+                scor = new Label();
+                scor.Content = 0;
+                scor.FontSize = 50;
+                scor.Width = 60;
+                scor.Height = 70;
+                scor.VerticalAlignment = VerticalAlignment.Top;
+                scor.HorizontalAlignment = HorizontalAlignment.Left;
+                scor.Margin = new Thickness(sleft, stop, 0, 0);
+                mriz.Children.Add(scor);
+            }
+            public static void scoreplus(int plus)
+            {
+                string sscoreusing = scor.Content.ToString();
+                int scoreusing = Convert.ToInt32(sscoreusing);
+                scor.Content = scoreusing + plus;
+            }
+        }
         int interval = 0;
+        int spawningtime = 0;
         int ts = 500;
         public static int doublejump = 0;
         public static bool right, left, jump;
         int gravity = 30;
         public static int force = 0;
         public static BitmapImage wallimg = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/testwall.png"));
+
+        public static List<UIElement> itemstoremove = new List<UIElement>();
+
         private void Clock(object sender, EventArgs e)
         {
+
+            ivan.echeck(mriz);
             if (right == true) { ivan.moveright(); }
             if (left == true) { ivan.moveleft(); }
             interval++;
+            spawningtime++;
             if (interval == 1000 / ts)
             {
                 ivan.jumpe(force);
                 interval = 0;
             }
+            if (spawningtime == 100)
+            {
+                EnType1 t1;
+                t1 = new EnType1(mriz);
+            }
+            if (spawningtime == 500)
+            {
+                EnType1 t2;
+                t2 = new EnType1(mriz);
+            }
+            if (spawningtime == 1000)
+            {
+                EnType1 t3;
+                t3 = new EnType1(mriz);
+            }
+
         }
 
         private void KeyDown1(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Right) { right = true; }
             if (e.Key == Key.Left) { left = true; }
-            if (e.Key == Key.Space&&doublejump<1)
+            if (e.Key == Key.Space && doublejump < 1)
             {
                 doublejump += 1;
                 MainWindow.force = gravity;
@@ -124,13 +181,75 @@ namespace The_game
                     wallleft = x,
                     wallright = x + sirka,
                     wallup = y,
+                    eontop = 0
                 };
                 MainWindow.wali.Add(save);
             }
         }
 
         //main character class 
+        public class enemy
+        {
+            public static void destroy(Grid mriz, enemlog gay)
+            {
+                int ix = gay.left;
+                int yp = gay.top;
+                foreach (FrameworkElement ui in itemstoremove)
+                {
+                    double x = ui.Margin.Left;
+                    double y = ui.Margin.Top;
+                    if (ix == x && y == yp)
+                    {
+                        itemstoremove.Remove(ui);
+                        mriz.Children.Remove(ui);
+                        scoreboard.scoreplus(1);
+                        break;
+                    }
+                }
+            }
+            public void elog(int x, int y, int sirka, int vyska)
+            {
+                enemlog save = new enemlog()
+                {
+                    left = x,
+                    top = y,
+                    right = x + sirka,
+                    bottom = y + vyska
+                };
+                MainWindow.enemiesave.Add(save);
+            }
+        }
+        public class EnType1 : enemy
+        {
+            public EnType1(Grid mriz)
+            {
+                Ellipse e1;
+                int onecreate = 0; // creat only one in row
+                e1 = new Ellipse();
+                e1.Width = 50;
+                e1.Height = 50;
+                e1.VerticalAlignment = VerticalAlignment.Top;
+                e1.HorizontalAlignment = HorizontalAlignment.Left;
+                e1.Fill = new SolidColorBrush(Colors.Black);
+                foreach (walliind wall in wali)
+                {
+                    if (wall.eontop == 0 && onecreate == 0)
+                    {
+                        onecreate = 1;
+                        wall.eontop = 1;
+                        e1.Margin = new Thickness((wall.wallright - wall.wallleft) / 2 + wall.wallleft - (e1.Width / 2), wall.wallup - e1.Width - 10, 0, 0);
+                        mriz.Children.Add(e1);
+                        int a = Convert.ToInt32((wall.wallright - wall.wallleft) / 2 + wall.wallleft - (e1.Width / 2));
+                        int b = Convert.ToInt32(wall.wallup - e1.Width - 10);
+                        elog(a, b, Convert.ToInt32(e1.Width), Convert.ToInt32(e1.Height));
+                        itemstoremove.Add(e1);
 
+
+                    }
+                }
+
+            }
+        }
         class character
         {
             BitmapImage charleft = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/characterpicrigleft.png"));
@@ -151,9 +270,21 @@ namespace The_game
                 c.Margin = new Thickness(50, /*MainWindow.workinggrid_height - MainWindow.char_height*/100, 0, 0);
                 mriz.Children.Add(c);
                 c.Fill = new ImageBrush(charright);
-
             }
-
+            public void echeck(Grid mriz)
+            {
+                int left = Convert.ToInt32(c.Margin.Left);
+                int up = Convert.ToInt32(c.Margin.Top);
+                foreach (enemlog gay in enemiesave)
+                {
+                    if (left + MainWindow.char_width > gay.left && up + MainWindow.char_height > gay.top && left < gay.right && up < gay.bottom)
+                    {
+                        enemy.destroy(mriz, gay);
+                        enemiesave.Remove(gay);
+                        break;
+                    }
+                }
+            }
             public void moveleft()
             {
                 bool inner = true;
@@ -190,7 +321,7 @@ namespace The_game
                         c.Margin = new Thickness(w.wallleft - MainWindow.char_width, up, 0, 0);
                     }
                 }
-                if (left + MainWindow.char_width > MainWindow.workinggrid_width-5)
+                if (left + MainWindow.char_width > MainWindow.workinggrid_width - 5)
                 {
                     inner = false;
                 }
@@ -212,13 +343,13 @@ namespace The_game
                 foreach (walliind w in wali)
                 {
                     //kontrola wall nad
-                    if (left+MainWindow.char_width>w.wallleft&&left<w.wallright&&up<w.walldown&&up+MainWindow.char_height>w.walldown)
+                    if (left + MainWindow.char_width > w.wallleft && left < w.wallright && up < w.walldown && up + MainWindow.char_height > w.walldown)
                     {
                         objectontop = true;
                         c.Margin = new Thickness(left, w.walldown, 0, 0);
                     }
                     //kontrola wall pod 
-                    if (up+MainWindow.char_height>=w.wallup&&left+MainWindow.char_width>w.wallleft&&left<w.wallright&&up<w.wallup)
+                    if (up + MainWindow.char_height >= w.wallup && left + MainWindow.char_width > w.wallleft && left < w.wallright && up < w.wallup)
                     {
                         objectondown = true;
                         if (force < 0)
@@ -228,9 +359,9 @@ namespace The_game
                     }
                 }
                 //kontrola border nad
-                if(objectondown == true)
+                if (objectondown == true)
                 {
-                    MainWindow.doublejump = 0; 
+                    MainWindow.doublejump = 0;
                     if (force > 0)
                     {
                         c.Margin = new Thickness(left, up - force, 0, 0);
@@ -240,7 +371,6 @@ namespace The_game
                         force = 0;
                         MainWindow.force = 0;
                     }
-
                 }
                 if (up < 0)
                 {
@@ -248,7 +378,7 @@ namespace The_game
                 }
                 if (objectontop == true)
                 {
-                     force = -3;
+                    force = -3;
                     MainWindow.force = -3;
                 }
                 //kontrola border pod
