b25ea7f0f304794f85b68428e5ef063273515ea8
simoncerman <cerman.s@seznam.cz>
2021-01-19 20:06:55 +0100

    Some more names edited

--- gamei2a/The game/MainWindow.xaml.cs
+++ gamei2a/The game/MainWindow.xaml.cs
@@ -51,54 +51,53 @@ namespace The_game
     {
         public static int workinggrid_width = 900;
         public static int workinggrid_height = 750;
-        character ivan;
-        DispatcherTimer time = new DispatcherTimer(DispatcherPriority.Render);
+        private readonly Character ivan;
+        readonly DispatcherTimer time = new DispatcherTimer(DispatcherPriority.Render);
         public MainWindow()
         {
             InitializeComponent();
             Scoreboard score;
-            wall wall1;
-            wall wall2;
-            wall wall3;
+            Wall wall1;
+            Wall wall2;
+            Wall wall3;
             time.Interval = new TimeSpan(0, 0, 0, 0, 1);
             time.Tick += Clock;
             time.Start();
             score = new Scoreboard(mriz, 700, 40);
-            ivan = new character(mriz);
-            wall1 = new wall(100, 300, 150, 50, mriz);
-            wall2 = new wall(600, 300, 150, 50, mriz);
-            wall3 = new wall(350, 500, 150, 50, mriz);
+            ivan = new Character(mriz);
+            wall1 = new Wall(100, 300, 150, 50, mriz);
+            wall2 = new Wall(600, 300, 150, 50, mriz);
+            wall3 = new Wall(350, 500, 150, 50, mriz);
         }
-        int ts = 500;
+        readonly int ts = 500;
         int interval = 0;
         int spawningtime = 0;
         public static int doublejump = 0;
         public static bool right, left, jump;
-        int gravity = 30;
+        readonly int gravity = 30;
         public static int force = 0;
         private void Clock(object sender, EventArgs e)
         {
-            if (right == true) { ivan.moveright(); }
-            if (left == true) { ivan.moveleft(); }
+            if (right == true) { ivan.Moveright(); }
+            if (left == true) { ivan.Moveleft(); }
             interval++;
             spawningtime++;
             if (interval == 1000 / ts)
             {
-                ivan.jumpe(force);
+                ivan.Jumpe(force);
                 interval = 0;
             }
             if (spawningtime == 150)
             {
-                EnType1 t1;
-                t1 = new EnType1(mriz);
+                EnType1 t1 = new EnType1(mriz);
                 spawningtime = 0;
             }
-            bullet.bulletchange(mriz);
+            Bullet.Bulletchange(mriz);
         }
-        private void mousemainclick(object sender, MouseButtonEventArgs e)
+        private void Mousemainclick(object sender, MouseButtonEventArgs e)
         {
-            bullet b;
-            b = new bullet(mriz);
+            Bullet b;
+            b = new Bullet(mriz);
         }
         private void KeyDown1(object sender, KeyEventArgs e)
         {
@@ -111,8 +110,8 @@ namespace The_game
             }
             if (e.Key == Key.F)
             {
-                bullet b;
-                b = new bullet(mriz);
+                Bullet b;
+                b = new Bullet(mriz);
             }
         }
         private void KeyUp1(object sender, KeyEventArgs e)
@@ -136,18 +135,18 @@ namespace The_game
             scor.Margin = new Thickness(sleft, stop, 0, 0);
             mriz.Children.Add(scor);
         }
-        public static void scoreplus(int plus)
+        public static void Scoreplus(int plus)
         {
             string sscoreusing = scor.Content.ToString();
             int scoreusing = Convert.ToInt32(sscoreusing);
             scor.Content = scoreusing + plus;
         }
     }
-    class wall
+    class Wall
     {
         public static List<Walliind> wali = new List<Walliind>();
         public Rectangle w;
-        public wall(int x, int y, int sirka, int vyska, Grid mriz)
+        public Wall(int x, int y, int sirka, int vyska, Grid mriz)
         {
             w = new Rectangle();
             w.Width = sirka;
@@ -158,10 +157,10 @@ namespace The_game
             w.HorizontalAlignment = HorizontalAlignment.Left;
             mriz.Children.Add(w);
             BitmapImage wallimg = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/testwall.png"));
-            jevois(x, y, sirka, vyska);
+            Jevois(x, y, sirka, vyska);
             w.Fill = new ImageBrush(wallimg);
         }
-        private static void jevois(int x, int y, int sirka, int vyska)
+        private static void Jevois(int x, int y, int sirka, int vyska)
         {
             Walliind save = new Walliind()
             {
@@ -172,15 +171,15 @@ namespace The_game
                 Eontop = 0,
                 Wasontop = false
             };
-            wall.wali.Add(save);
+            Wall.wali.Add(save);
         }
     }
-    class enemy
+    class Enemy
     {
         public static List<UIElement> itemstoremove = new List<UIElement>();
         public static List<Enemlog> enemiesave = new List<Enemlog>();
         public static int destroycount = 0;
-        public static void destroy(Grid mriz, Enemlog gay)
+        public static void Destroy(Grid mriz, Enemlog gay)
         {
             int ix = gay.Left;
             int yp = gay.Top;
@@ -190,7 +189,7 @@ namespace The_game
                 double y = ui.Margin.Top;
                 if (ix == x && y == yp)
                 {
-                    foreach (Walliind w in wall.wali)
+                    foreach (Walliind w in Wall.wali)
                     {
                         if (w.Wallleft == gay.Wl && w.Wallup == gay.Wt)
                         {
@@ -199,13 +198,13 @@ namespace The_game
                     }
                     itemstoremove.Remove(ui);
                     mriz.Children.Remove(ui);
-                    Scoreboard.scoreplus(1);
+                    Scoreboard.Scoreplus(1);
                     destroycount++;
                     break;
                 }
             }
         }
-        public void elog(int x, int y, int sirka, int vyska, int wll, int wtt)
+        public void Elog(int x, int y, int sirka, int vyska, int wll, int wtt)
         {
             Enemlog save = new Enemlog()
             {
@@ -216,10 +215,10 @@ namespace The_game
                 Wl = wll,
                 Wt = wtt,
             };
-            enemy.enemiesave.Add(save);
+            Enemy.enemiesave.Add(save);
         }
     }
-    class EnType1 : enemy
+    class EnType1 : Enemy
     {
         public EnType1(Grid mriz)
         {
@@ -233,7 +232,7 @@ namespace The_game
             e1.VerticalAlignment = VerticalAlignment.Top;
             e1.HorizontalAlignment = HorizontalAlignment.Left;
             e1.Fill = new SolidColorBrush(Colors.Black);
-            foreach (Walliind wall in wall.wali)
+            foreach (Walliind wall in Wall.wali)
             {
                 if (wall.Eontop == 0 && onecreate == 0 && wall.Wasontop == false)
                 {
@@ -245,11 +244,11 @@ namespace The_game
                     mriz.Children.Add(e1);
                     int a = Convert.ToInt32((wall.Wallright - wall.Wallleft) / 2 + wall.Wallleft - (e1.Width / 2));
                     int b = Convert.ToInt32(wall.Wallup - e1.Height);
-                    elog(a, b, Convert.ToInt32(e1.Width), Convert.ToInt32(e1.Height), wall.Wallleft, wall.Wallup);
+                    Elog(a, b, Convert.ToInt32(e1.Width), Convert.ToInt32(e1.Height), wall.Wallleft, wall.Wallup);
                     itemstoremove.Add(e1);
                 }
             }
-            foreach (Walliind walii in wall.wali)
+            foreach (Walliind walii in Wall.wali)
             {
                 if (walii.Wasontop == true)
                 {
@@ -258,7 +257,7 @@ namespace The_game
             }
             if (ontruetest == 3)
             {
-                foreach (Walliind walis in wall.wali)
+                foreach (Walliind walis in Wall.wali)
                 {
                     walis.Wasontop = false;
                     ontruetest = 0;
@@ -266,7 +265,7 @@ namespace The_game
             }
         }
     }
-    class EnType2 : enemy
+    class EnType2 : Enemy
     {
         public EnType2(Grid mriz)
         {
@@ -278,7 +277,7 @@ namespace The_game
             e2.HorizontalAlignment = HorizontalAlignment.Left;
             e2.VerticalAlignment = VerticalAlignment.Top;
             e2.Fill = new SolidColorBrush(Colors.Violet);
-            foreach (Walliind wall in wall.wali)
+            foreach (Walliind wall in Wall.wali)
             {
                 if (wall.Eontop == 0 && onecreate == 0)
                 {
@@ -288,17 +287,17 @@ namespace The_game
                     mriz.Children.Add(e2);
                     int a = Convert.ToInt32((wall.Wallright - wall.Wallleft) / 2 + wall.Wallleft - (e2.Width / 2));
                     int b = Convert.ToInt32(wall.Wallup - e2.Width - 10);
-                    elog(a, b, Convert.ToInt32(e2.Width), Convert.ToInt32(e2.Height), wall.Wallleft, wall.Wallup);
+                    Elog(a, b, Convert.ToInt32(e2.Width), Convert.ToInt32(e2.Height), wall.Wallleft, wall.Wallup);
                     itemstoremove.Add(e2);
                 }
             }
         }
     }
-    class bullet
+    class Bullet
     {
         public static List<Buletinf> bulletlog = new List<Buletinf>();
         public Rectangle bul;
-        public bullet(Grid mriz)
+        public Bullet(Grid mriz)
         {
             bul = new Rectangle();
             bul.Width = 10;
@@ -306,19 +305,19 @@ namespace The_game
             bul.Fill = new SolidColorBrush(Colors.Red);
             bul.VerticalAlignment = VerticalAlignment.Top;
             bul.HorizontalAlignment = HorizontalAlignment.Left;
-            bul.Margin = new Thickness(character.charposleft + 20, character.charposttop + 40, 0, 0);
+            bul.Margin = new Thickness(Character.charposleft + 20, Character.charposttop + 40, 0, 0);
             mriz.Children.Add(bul);
             Buletinf bullet = new Buletinf
             {
                 Left = Convert.ToInt32(bul.Margin.Left),
                 Top = Convert.ToInt32(bul.Margin.Top),
-                Side = character.charonright,
+                Side = Character.charonright,
                 Ue = bul,
                 El = bul
             };
             bulletlog.Add(bullet);
         }
-        public static void bulletchange(Grid mriz)
+        public static void Bulletchange(Grid mriz)
         {
             foreach (Buletinf bullet in bulletlog)
             {
@@ -342,15 +341,15 @@ namespace The_game
                     }
                     mriz.Children.Add(save);
 
-                    foreach (Enemlog gay in enemy.enemiesave)
+                    foreach (Enemlog gay in Enemy.enemiesave)
                     {
                         if (bullet.Left + bullet.El.Width > gay.Left &&
                             bullet.Top + bullet.El.Height > gay.Top &&
                             bullet.Left < gay.Right &&
                             bullet.Top < gay.Bottom)
                         {
-                            enemy.destroy(mriz, gay);
-                            enemy.enemiesave.Remove(gay);
+                            Enemy.Destroy(mriz, gay);
+                            Enemy.enemiesave.Remove(gay);
                             break;
                         }
                     }
@@ -359,18 +358,18 @@ namespace The_game
             }
         }
     }
-    class character
+    class Character
     {
-        int go_down_speed = 3;
-        int char_width = 80; //šířka hráče 
-        int char_height = 80; //výška hráče 
+        readonly int go_down_speed = 3;
+        readonly int char_width = 80; //šířka hráče 
+        readonly int char_height = 80; //výška hráče 
         public static bool charonright = true;
         public static int charposleft = 0;
         public static int charposttop = 0;
-        BitmapImage charleft = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/charleft.png"));
-        BitmapImage charright = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/charright.png"));
-        Rectangle c;
-        public character(Grid mriz)
+        readonly BitmapImage charleft = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/charleft.png"));
+        readonly BitmapImage charright = new BitmapImage(new Uri("http://dod.vos-sps-jicin.cz/wp-content/uploads/simnsgame/charright.png"));
+        readonly Rectangle c;
+        public Character(Grid mriz)
         {
             Image charac1 = new Image();
             charac1.Width = char_width;
@@ -386,26 +385,26 @@ namespace The_game
             mriz.Children.Add(c);
             c.Fill = new ImageBrush(charright);
         }
-        public void echeck(Grid mriz)
+        public void Echeck(Grid mriz)
         {
             int left = Convert.ToInt32(c.Margin.Left);
             int up = Convert.ToInt32(c.Margin.Top);
-            foreach (Enemlog gay in enemy.enemiesave)
+            foreach (Enemlog gay in Enemy.enemiesave)
             {
                 if (left + char_width > gay.Left && up + char_height > gay.Top && left < gay.Right && up < gay.Bottom)
                 {
-                    enemy.destroy(mriz, gay);
-                    enemy.enemiesave.Remove(gay);
+                    Enemy.Destroy(mriz, gay);
+                    Enemy.enemiesave.Remove(gay);
                     break;
                 }
             }
         }
-        public void moveleft()
+        public void Moveleft()
         {
             bool inner = true;
             int left = Convert.ToInt32(c.Margin.Left);
             int up = Convert.ToInt32(c.Margin.Top);
-            foreach (Walliind w in wall.wali)
+            foreach (Walliind w in Wall.wali)
             {
                 if (left - 1 < w.Wallright && up < w.Walldown && up + char_height > w.Wallup && left + char_width > w.Wallright)
                 {
@@ -426,12 +425,12 @@ namespace The_game
             charposleft = Convert.ToInt32(c.Margin.Left);
             charposttop = Convert.ToInt32(c.Margin.Top);
         }
-        public void moveright()
+        public void Moveright()
         {
             bool inner = true;
             int left = Convert.ToInt32(c.Margin.Left);
             int up = Convert.ToInt32(c.Margin.Top);
-            foreach (Walliind w in wall.wali)
+            foreach (Walliind w in Wall.wali)
             {
                 if (left + 1 + char_width > w.Wallleft && up < w.Walldown && up + char_height > w.Wallup && left < w.Wallleft)
                 {
@@ -452,7 +451,7 @@ namespace The_game
             charposleft = Convert.ToInt32(c.Margin.Left);
             charposttop = Convert.ToInt32(c.Margin.Top);
         }
-        public void jumpe(int force)
+        public void Jumpe(int force)
         {
             bool objectontop = false;
             bool objectondown = false;
@@ -461,7 +460,7 @@ namespace The_game
             int left = Convert.ToInt32(c.Margin.Left);
             int up = Convert.ToInt32(c.Margin.Top);
 
-            foreach (Walliind w in wall.wali)
+            foreach (Walliind w in Wall.wali)
             {
                 if (left + char_width > w.Wallleft && left < w.Wallright && up < w.Walldown && up + char_height > w.Walldown)
                 {
